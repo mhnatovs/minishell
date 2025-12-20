d1a83990b2e0e99bcb8ebb1aac1044e7a4fdb287
mhnatovs <mariana.hnatovska@gmail.com>
2025-12-20 15:27:21 +0200

    Add files via upload

---
+++ minishell.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhnatovs <mhnatovs@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/19 16:15:25 by mhnatovs          #+#    #+#             */
+/*   Updated: 2025/12/19 16:21:07 by mhnatovs         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <sys/types.h>
+# include <string.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include "libft/libft.h"
+
+#endif
