b26f01febc23af7aad5753931e85ee1eca0a5c1f
Marianna Hnatovska <mhnatovs@2-C-2.42heilbronn.de>
2025-12-23 20:03:07 +0100

    expanded 0

--- main.c
+++ main.c
@@ -6,34 +6,86 @@
 /*   By: mhnatovs <mhnatovs@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/19 16:15:14 by mhnatovs          #+#    #+#             */
-/*   Updated: 2025/12/22 18:25:35 by mhnatovs         ###   ########.fr       */
+/*   Updated: 2025/12/23 20:00:40 by mhnatovs         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-int g_status;
+
+int	g_status;
+
 void	execute(char **args, char **envp)
 {
 	char	*cmd_path;
 	pid_t	pid;
+	int		status;
 
 	cmd_path = find_cmd_path(args[0], envp);
+	if (!cmd_path)
+	{
+		printf("%s: command not found\n", args[0]);
+		g_status = 127;
+		return ;
+	}
 	pid = fork();
 	if (pid == 0)
 	{
-		execve(cmd_path, &args[0], envp);
+		execve(cmd_path, args, envp);
+		perror("execve");
+		exit(126);
+	}
+	else if (pid > 0)
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			g_status = WEXITSTATUS(status);
 	}
 }
+int	count_args(char **args)
+{
+	int		i;
+
+	i = 0;
+	while (args[i])
+		i++;
+	return (i);
+}
+char	*expanded_arg(char *arg)
+{
+	if (ft_strcmp(arg, "$?") == 0)
+		return ft_itoa(g_status);
+	return (ft_strdup(arg));
+}
+
+char	**expanded_args(char **args)
+{
+	int		i;
+	char	**ptrs;
+	int		size;
+
+	i = 0;
+	size = count_args(args);
+	ptrs = malloc(sizeof(char *) * size + 1);
+	if (!ptrs)
+		return (NULL);
+	while (args[i])
+	{
+		ptrs[i] = expanded_arg(args[i]);
+		i++;
+	}
+	ptrs[i] = NULL;
+	return (ptrs);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	char	*line;
 	char	**args;
+	char	**expanded;
 
 	(void) argv;
 	if (argc != 1)
 		return (1);
-		// if (ft_strcmp(argv[1], "./minishell") == 0)
-		// {
 	while (1)
 	{
 		line = readline("minishell$ ");
@@ -48,14 +100,22 @@ int	main(int argc, char **argv, char **envp)
 		if (!args || !args[0])
 		{
 			free_split(args);
+			free(line);
 			continue ;
 		}
-		
-		// printf("%s\n", cmd_path);
+		expanded = expanded_args(args);
+		if (!expanded || !expanded[0])
+		{
+			free_split(expanded);
+			free_split(args);
+			free(line);
+			continue ;
+		}
+		execute(expanded, envp);
 		free_split(args);
 		free(line);
+		printf("DEBUG g_status = %d\n", g_status);
 	}
-	// }
 	return (0);
 }
 
@@ -74,7 +134,6 @@ int	main(int argc, char **argv, char **envp)
 //         i++;
 //     }
 // }
-
 // int main(int argc, char **argv, char **envp)
 // {
 // 	char *path;
@@ -89,20 +148,10 @@ int	main(int argc, char **argv, char **envp)
 // 	// 	printf("PATH not found\n");
 // 	// debug_path(envp);
 // 	char *cmd_path = find_cmd_path(argv[1], envp);
-
 // 		printf("%s\n", cmd_path);
-
-		
 // 	return 0;
 // }
-
 /*
-
-PATH=/home/mhnatovs/goinfre/homebrew/bin:
-/home/mhnatovs/bin:/home/mhnatovs/bin:
-/usr/local/sbin:/usr/local/bin:/usr/sbin:
-/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-
 readline
 rl_clear_history
 rl_on_new_line
